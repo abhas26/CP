d58a50b99b003f31a588ba427b6c5cce6f21b830
PratyashaKar <56728953+PratyashaKar@users.noreply.github.com>
2021-10-29 23:05:50 +0530

    Create Square root.cpp

---
+++ Square root.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+
+while(t--){
+    
+    int n;
+    cin>>n;
+    
+    int ans=(int)sqrt(n);
+    
+    cout<<ans<<endl;
+  }
+  
+}
